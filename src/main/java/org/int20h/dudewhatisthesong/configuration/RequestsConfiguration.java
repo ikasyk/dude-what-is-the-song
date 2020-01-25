0131b86e1a2a2e36cf70835c451b5787bbb4241d
ikasyk <kasyk3@gmail.com>
2020-01-25 10:50:42 +0200

    CORS fix

--- src/main/java/org/int20h/dudewhatisthesong/configuration/RequestsConfiguration.java
+++ src/main/java/org/int20h/dudewhatisthesong/configuration/RequestsConfiguration.java
@@ -4,11 +4,12 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.Duration;
 
@@ -29,5 +30,16 @@ public class RequestsConfiguration {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*").allowCredentials(true);
+            }
+        };
     }
 }
